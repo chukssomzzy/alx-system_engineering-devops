4d6e9f29befd10eb5b5d54f64f80ada5fca3082d
chukssomzzy <chukssomzzy19@gmail.com>
2023-07-24 12:43:06 +0100

    csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""For a given employee ID, returns information about his/her TODO list
+progress"""
+import requests
+from sys import argv
+import csv
+
+try:
+    id = int(argv[1])
+    url_user_task = f'https://jsonplaceholder.typicode.com/users/{id}/todos'
+    url_user = f'https://jsonplaceholder.typicode.com/users/{id}'
+    r_task = requests.get(url_user_task)
+    r_user = requests.get(url_user)
+    employee_tasks = r_task.json()
+    employee_name = r_user.json().get('name')
+    task_comp = 0
+    total_task = 0
+    completed_task_title = []
+    with open(f'{id}.csv', 'w', newline='') as csvfile:
+        taskwriter = csv.writer(csvfile, delimiter=',',
+                                quotechar='"', quoting=csv.QUOTE_ALL)
+        for task in employee_tasks:
+            taskwriter.writerow(
+                [id, employee_name, task.get("completed"), task.get("title")])
+except Exception as e:
+    print(e)
+    exit(1)
+else:
+    exit(0)
