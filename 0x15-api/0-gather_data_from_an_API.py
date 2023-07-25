80c231720facf76579eac51e18ab7ad4723477e3
chukssomzzy <chukssomzzy19@gmail.com>
2023-07-25 04:54:10 +0000

    not order

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,9 +2,9 @@
 """For a given employee ID, returns information about his/her TODO list
 progress"""
 
-from sys import argv
-
 import requests
+
+from sys import argv
 
 if __name__ == "__main__":
     try:
