bf4b173fc290a9be0824b2d8fc589791a19d60da
chukssomzzy <chukssomzzy19@gmail.com>
2023-08-09 23:52:55 +0000

    hope works

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,10 @@
+# change the wrong extention name for wp-setting.php 
+
+include stdlib
+
+file_line { '/var/www/html/wp-settings.php':
+  ensure => present,
+  path   => '/var/www/html/wp-settings.php',
+  line   => "require_once( ABSPATH . WPINC . '/class-wp-locale.php);",
+  match  => "^require_once(.*'/class-wp-locale.phpp'.*);$",
+}
