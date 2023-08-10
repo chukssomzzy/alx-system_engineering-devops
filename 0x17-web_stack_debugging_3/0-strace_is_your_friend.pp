2fab77f2ff4655772f653a8e5da1e86d20ad3460
chukssomzzy <chukssomzzy19@gmail.com>
2023-08-10 01:40:29 +0000

    fix wordpress

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,6 +2,6 @@
 
 exec { 'fix-wordpress':
   path    => '/bin:/usr/bin',
-  command => "sed -i '/require_once(.*class-wp-locale/s/.phpp/php/1' /var/www/html/wp-settings.php",
+  command => "sed -i '/require_once(.*class-wp-locale/s/.phpp/.php/1' /var/www/html/wp-settings.php",
   onlyif  => "grep -q '^require_once(.*class-wp-locale.phpp.*' /var/www/html/wp-settings.php",
 }
