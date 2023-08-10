ccde347d3c2d4b436d95253073c4daf7e9ed65cd
chukssomzzy <chukssomzzy19@gmail.com>
2023-08-10 01:09:27 +0000

    puppet file_line

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,12 +1,9 @@
 # change the wrong extention name for wp-setting.php 
 
 
-file { '/var/www/html/wp-settings.php':
-  ensure  => present,
-  path    => '/var/www/html/wp-settings.php',
-  content =>  replace(
-    file('/var/www/html/wp-settings.php'),
-    "require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );",
-    "require_once( ABSPATH . WPINC . '/class-wp-locale.php')"
-  )
+file_line { '/var/www/html/wp-settings.php':
+  ensure => present,
+  path   => '/var/www/html/wp-settings.php',
+  line   => "require_once( ABSPATH . WPINC . '/class-wp-locale.php);",
+  match  => "^require_once(.*'/class-wp-locale.phpp'.*);$",
 }
