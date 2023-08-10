a5f53bd0f8e49ba2641d5cf7b11803e48a3fe2b4
chukssomzzy <chukssomzzy19@gmail.com>
2023-08-10 01:32:23 +0000

    grep

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,7 +2,6 @@
 
 exec { 'replace_line':
   path    => '/bin/bash',
-  command => "sed -i 's/^require_once(.*class-wp-locale.phpp.*);$/require_once( ABSPATH . WPINC . \"\/class-wp-locale.php\");/1' 
-  /var/www/html/wp-settings.php",
-  onlyif  => "grep -q '^require_once(.*class-wp-locale.phpp.*);$/' /var/www/html/wp-settings.php",
+  command => "sed -i '/require_once(.*class-wp-locale/s/.phpp/php/1' /var/www/html/wp-settings.php",
+  onlyif  => "grep -q '^require_once(.*class-wp-locale.phpp.*' /var/www/html/wp-settings.php",
 }
