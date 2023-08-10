d08ce4d5e02000da5488b4f962b7ce4b9e3b7ee8
chukssomzzy <chukssomzzy19@gmail.com>
2023-08-10 01:33:53 +0000

    path

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
 # change the wrong extention name for wp-setting.php 
 
 exec { 'replace_line':
-  path    => '/bin/bash',
+  path    => '/bin:/usr/bin',
   command => "sed -i '/require_once(.*class-wp-locale/s/.phpp/php/1' /var/www/html/wp-settings.php",
   onlyif  => "grep -q '^require_once(.*class-wp-locale.phpp.*' /var/www/html/wp-settings.php",
 }
