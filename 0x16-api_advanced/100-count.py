55a6158b62589f40225f24bdb7e2436c60824910
chukssomzzy <chukssomzzy19@gmail.com>
2023-08-11 06:09:56 +0000

    sorted

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -12,18 +12,27 @@ def count_words(subreddit, word_list, after=None):
     headers = {"User-Agent": "chukssomzzy-app/v0.0.1"}
     r = requests.get(url, params=params, headers=headers,
                      allow_redirects=False)
-    if type(word_list) == str:
+    if type(word_list) == list:
         word_list_copy = {}
         for word in word_list:
-            word_list_copy[word] = 0
+            word_list_copy[word + " "] = 0
         word_list = word_list_copy
     if r.status_code == 200:
         after = r.json()["data"]["after"]
         hot_articles = r.json()["data"]["children"]
         for article in hot_articles:
+            article_title = article["data"]["title"]
             for word in word_list:
-                while article.find(word) > -1:
-                    article = article[article.find(word):]
+                word_index = article_title.find(word)
+                while word_index != -1:
+                    article_title = article_title[(word_index + len(word)):]
                     word_list[word] += 1
-        for word, val in word_list.items():
-            print(f"{word}: {val}")
+                    word_index = article_title.find(word)
+        if after:
+            count_words(subreddit, word_list, after)
+        else:
+            for word, val in sorted(word_list.items(),
+                                    key=lambda key_val: key_val[1],
+                                    reverse=True):
+                if val != 0:
+                    print(f"{word.strip()}: {val}")
