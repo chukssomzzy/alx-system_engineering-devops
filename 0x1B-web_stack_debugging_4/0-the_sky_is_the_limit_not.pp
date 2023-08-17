49183bf06da5415d1c95cbe792f1cf70db16a1d5
chukssomzzy <chukssomzzy19@gmail.com>
2023-08-17 14:48:24 +0100

    regex test

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,8 @@
+-- Fix issue ulimit issue in default nginx file 
+
+exec { 'fix--for--nginx':
+  path  => ['/usr/bin', '/bin'],
+  cwd   => '/etc/default',
+  group => 'sudo',
+  command => 'sed -i "/ULIMIT=\"-n /s/\d+/5000/1\"" nginx'
+  }                         
