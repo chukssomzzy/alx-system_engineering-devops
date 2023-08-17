4ce67756360f7e566e3af30048ddd5bd5bfc3a9b
chukssomzzy <chukssomzzy19@gmail.com>
2023-08-17 15:01:48 +0100

    wrong comment

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,8 @@
--- Fix issue ulimit issue in default nginx file 
+# Fix issue ulimit issue in default nginx file
 
 exec { 'fix--for--nginx':
-  path  => ['/usr/bin', '/bin'],
-  cwd   => '/etc/default',
-  group => 'sudo',
+  path    => ['/usr/bin', '/bin'],
+  cwd     => '/etc/default',
+  group   => 'sudo',
   command => 'sed -i "/ULIMIT=\"-n /s/\d+/5000/1\"" nginx'
-  }                         
+  }
