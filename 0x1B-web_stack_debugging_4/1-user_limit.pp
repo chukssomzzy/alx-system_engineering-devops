3fe48c364c9dc95dc6508f82c13a3e83b6d46e11
chukssomzzy <chukssomzzy19@gmail.com>
2023-08-17 16:08:52 +0000

    holberton

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,8 @@
+# change ulimit for a user os level 
+
+exec { 'change-os-configuration-for-holberton-user':
+  path    => ['/usr/bin', '/bin'],
+  group   => 'sudo',
+  cwd     => '/etc/security',
+  command => 'sed -ri "/\>holberton*?[[:digit:]]+\>/ s/[[:digit:]]+/30000" limits.conf'
+}
