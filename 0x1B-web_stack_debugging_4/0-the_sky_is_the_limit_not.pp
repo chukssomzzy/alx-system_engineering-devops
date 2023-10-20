e53ac2c7b9f70b42af2cc17d0fd0728b2ee9eb6b
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2023-10-20 20:18:07 +0000

    quotes

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # increase the ulimit of nginx process 
 
 exec { 'fix--for-nginx':
-  command => `sed -i '/ULIMIT=\"-n 15\"/ s/15/4096/1' /etc/nginx/default`,
+  command => 'sed -i "/ULIMIT=\"-n 15\"/ s/15/4096/1" /etc/nginx/default',
   cwd     => '/etc/default',
   path    => '/bin',
 }
