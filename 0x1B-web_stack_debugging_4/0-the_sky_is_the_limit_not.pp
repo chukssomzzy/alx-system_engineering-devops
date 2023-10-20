b5bd323058bcb7cc727740d3a80506803595a448
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2023-10-20 20:24:51 +0000

    file not found fixed

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # increase the ulimit of nginx process 
 
 exec { 'fix--for-nginx':
-  command => 'sed -i "/ULIMIT=\"-n 15\"/ s/15/4096/1" /etc/nginx/default',
+  command => 'sed -i "/ULIMIT=\"-n 15\"/ s/15/4096/1" /etc/default/nginx',
   cwd     => '/etc/default',
   path    => '/bin',
 }
