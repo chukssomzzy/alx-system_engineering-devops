475a4ec8a238b5382f346d739f588ab50f11b5d5
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2023-10-21 17:54:16 +0000

    syntax error using quotation

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,10 @@
 # change open file limit for user holberton 
 
-# exec { 'change-os-configuration-for-holberton-user':
-#   'command' => 'sed -i "/holberton .*nofile/ s/[0-9]/65233/" limits.conf',
-#   'path'    => '/bin',
-#   'cwd'     => '/etc/security',
-# }
+exec { 'change-os-configuration-for-holberton-user':
+  command => 'sed -i "/holberton .*nofile/ s/[0-9]/65233/" limits.conf',
+  path    => '/bin',
+  cwd     => '/etc/security',
+}
 
  exec { 'fix--for-nginx':
   command => 'sed -i "/ULIMIT=\"-n 15\"/ s/15/4096/1" /etc/default/nginx',
