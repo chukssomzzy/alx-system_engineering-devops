7457ed06896841492734f7275683ab99aa118613
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2023-10-21 17:35:13 +0000

    wrong format fixed

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,6 @@
 # change open file limit for user holberton 
 
 exec { 'change-os-configuration-for-holberton-user':
-  'command' => '/bin/sed -i "/holberton .*nofile/ s/[0-9]/65233/" /etc/security/limits.conf"
+  'command' => '/bin/sed -i "/holberton .*nofile/ s/[0-9]/65233/" /etc/security/limits.conf",
   'path'    => '/bin'
 }
